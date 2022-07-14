78154f1bdf331d458175e0d3548e3176585c6bd2
Faister Cabrera Carvalho <Tex.Killer@hotmail.com>
2022-07-13 21:43:39 -0300

    Small change

--- free/gke.tf
+++ free/gke.tf
@@ -3,14 +3,43 @@ provider "google" {
   region  = var.location
 }
 
+resource "google_compute_network" "network" {
+  name                    = "network"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "subnetwork" {
+  name          = "subnetwork"
+  ip_cidr_range = "10.2.0.0/16"
+  region        = var.location
+  network       = google_compute_network.network.id
+  secondary_ip_range {
+    range_name    = "cluster-range"
+    ip_cidr_range = "192.168.1.0/24"
+  }
+
+  secondary_ip_range {
+    range_name    = "services-range"
+    ip_cidr_range = "192.168.64.0/22"
+  }
+}
+
 resource "google_container_cluster" "cluster" {
   name                      = var.cluster_name
   location                  = var.location
-  subnetwork                = var.subnetwork
   remove_default_node_pool  = true
+  default_max_pods_per_node = 110
   logging_service           = "none"
   monitoring_service        = "none"
   initial_node_count        = 1
+
+  network    = google_compute_network.network.id
+  subnetwork = google_compute_subnetwork.subnetwork.id
+
+  ip_allocation_policy {
+    cluster_secondary_range_name  = google_compute_subnetwork.subnetwork.secondary_ip_range.0.range_name
+    services_secondary_range_name = google_compute_subnetwork.subnetwork.secondary_ip_range.1.range_name
+  }
 }
 
 resource "google_container_node_pool" "pool" {
